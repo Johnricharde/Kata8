6a76754ffa55129a3fecb0a2075a767bff3603a0
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-14 17:06:17 +0100

    Add project files.

---
+++ Kata8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Kata8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static char FindMissingLetter(char[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i + 1] - array[i] > 1)
+                    return (char)(array[i] + 1);
+            }
+            return ' ';
+        }
+// Write a method that takes an array of consecutive(increasing) letters as
+// input and that returns the missing letter in the array.
+
+// You will always get an valid array.
+// And it will be always exactly one letter be missing.
+// The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+    }
+}
